9b736977bb3a1c675af7e0eb5de55e7b05479606
918pitman <918pitman@gmail.com>
2022-01-08 13:07:32 -0600

    update ip

--- Scripts/client.py
+++ Scripts/client.py
@@ -2,7 +2,7 @@
 
 import socket
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
+HOST = '192.168.0.234'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 def client():
--- Scripts/server.py
+++ Scripts/server.py
@@ -3,7 +3,7 @@
 import threading
 import socket
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+HOST = '192.168.0.234'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 def server():
