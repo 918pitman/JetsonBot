e0be61c18f257cba8a26107f44e465e7a8657a4f
918pitman <918pitman@gmail.com>
2022-01-08 20:23:45 -0600

    Update server.py

--- Scripts/server.py
+++ Scripts/server.py
@@ -2,6 +2,7 @@
 
 from threading import Thread
 import socket
+from typing_extensions import TypeGuard
 
 HOST = '143.198.161.181'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
@@ -22,8 +23,9 @@ def server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
-        conn, addr = s.accept()
-        Thread(target=chat, args=(conn,addr)).start()
+        while True:
+            conn, addr = s.accept()
+            Thread(target=chat, args=(conn,addr)).start()
 
 
 if __name__ == '__main__':
