871f2928787376eea3c5ade1e623e3dc4e9dae09
918pitman <918pitman@gmail.com>
2022-01-09 12:34:41 -0600

    Update client.py

--- Scripts/client.py
+++ Scripts/client.py
@@ -10,20 +10,21 @@ PORT = 65432
 def client():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         #s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # if platform.system() == 'Linux':
+        #     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
         s.connect((HOST, PORT))
         priv_addr = s.getsockname()
         print('Private Address is : ', priv_addr)
-        # if platform.system() == 'Linux':
-        #     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        s.sendall('Peer'.encode('utf-8'))
+        data = s.recv(1024)
+        resp = data.decode('utf-8')
+        if resp == 'First':
+            print('I connected to the server first!')
+        elif resp == 'Second':
+            print('I connected to the server second!')
+        else:
+            print('Access Denied')
         
-        while True:
-            msgout = input('To Server: ')
-            if msgout == 'q':
-                break
-            s.sendall(msgout.encode('utf-8'))
-            data = s.recv(1024)
-            msgin = data.decode('utf-8')
-            print('Received: ', msgin)
 
 if __name__ == '__main__':
     client()
