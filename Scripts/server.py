47450bb7b538fa27229fa0d2fc5daa2de42c235b
918pitman <918pitman@gmail.com>
2022-01-08 22:32:11 -0600

    Update server.py

--- Scripts/server.py
+++ Scripts/server.py
@@ -3,6 +3,8 @@ import platform
 import socket
 from threading import Thread
 
+from client import client
+
 HOST = '0.0.0.0'
 PORT = 65432
 
@@ -19,13 +21,19 @@ def chat(conn, addr):
 
 
 def server():
+    peers = []
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         #s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((HOST, PORT))
         s.listen()
-        while True:
+        while len(peers) <= 2:
             conn, addr = s.accept()
-            Thread(target=chat, args=(conn,addr)).start()
+            with conn:
+                peers.append(addr)
+        print('Connected to two peers')
+        print('Peer 1: ', client[0])
+        print('Peer 2: ', client[1])
+            
 
 
 if __name__ == '__main__':
