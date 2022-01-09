f0a3468a41a79b8852d59fdb01d5e640b7c179d4
918pitman <918pitman@gmail.com>
2022-01-09 13:39:15 -0600

    test udp

--- Scripts/udp_client.py
+++ Scripts/udp_client.py
@@ -1,25 +1,61 @@
-import logging
 import socket
 import sys
-from util import *
+import threading
 
-logger = logging.getLogger()
+rendezvous = ('147.182.184.215', 55555)
 
+# connect to rendezvous
+print('connecting to rendezvous server')
 
-def main(host='127.0.0.1', port=9999):
-    sock = socket.socket(socket.AF_INET, # Internet
-                         socket.SOCK_DGRAM) # UDP
-    sock.sendto(b'0', (host, port))
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('0.0.0.0', 50001))
+sock.sendto(b'0', rendezvous)
+
+while True:
+    data = sock.recv(1024).decode()
+
+    if data.strip() == 'ready':
+        print('checked in with server, waiting')
+        break
+
+data = sock.recv(1024).decode()
+ip, sport, dport = data.split(' ')
+sport = int(sport)
+dport = int(dport)
+
+print('\ngot peer')
+print('  ip:          {}'.format(ip))
+print('  source port: {}'.format(sport))
+print('  dest port:   {}\n'.format(dport))
+
+# punch hole
+# equiv: echo 'punch hole' | nc -u -p 50001 x.x.x.x 50002
+print('punching hole')
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('0.0.0.0', sport))
+sock.sendto(b'0', (ip, dport))
+
+print('ready to exchange messages\n')
+
+# listen for
+# equiv: nc -u -l 50001
+def listen():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('0.0.0.0', sport))
 
     while True:
-        data, addr = sock.recvfrom(1024)
-        print('client received: {} {}'.format(addr, data))
-        addr = msg_to_addr(data)
-        sock.sendto(b'0', addr)
-        data, addr = sock.recvfrom(1024)
-        print('client received: {} {}'.format(addr, data))
+        data = sock.recv(1024)
+        print('\rpeer: {}\n> '.format(data.decode()), end='')
 
+listener = threading.Thread(target=listen, daemon=True);
+listener.start()
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
-    main(*addr_from_args(sys.argv))
+# send messages
+# equiv: echo 'xxx' | nc -u -p 50002 x.x.x.x 50001
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('0.0.0.0', dport))
+
+while True:
+    msg = input('> ')
+    sock.sendto(msg.encode(), (ip, sport))
